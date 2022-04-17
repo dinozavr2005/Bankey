0d2c18a39a4cd470abd587f7f0ce9579723b60b8
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2022-04-17 11:13:51 +0300

    feat: Add LogouButton

---
+++ Bankey/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by Владимир on 17.04.2022.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
---
+++ Bankey/Utils/UITeextField+SecureToggle.swift
@@ -0,0 +1,27 @@
+//
+//  UITeextField+SecureToggle.swift
+//  Bankey
+//
+//  Created by Владимир on 17.04.2022.
+//
+
+import UIKit
+
+let passwordToggleButton = UIButton(type: .custom)
+
+extension UITextField {
+    
+    func enablePasswordToggle() {
+        passwordToggleButton.setImage(UIImage(systemName: "eye.fill"), for: .normal)
+        passwordToggleButton.setImage(UIImage(systemName: "eye.slash.fill"), for: .selected)
+        passwordToggleButton.addTarget(self, action: #selector(togglePasswordView), for: .touchUpInside)
+        
+        rightView = passwordToggleButton
+        rightViewMode = .always
+    }
+    
+    @objc func togglePasswordView(_ sender: Any) {
+        isSecureTextEntry.toggle()
+        passwordToggleButton.isSelected.toggle()
+    }
+}
